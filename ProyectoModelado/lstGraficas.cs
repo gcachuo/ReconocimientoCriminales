69244f37af0eb72b7213ce91ea51003b47fb5072
gcachuo <gcachu.o@gmail.com>
2014-11-26 23:32:21 -0600

    Basico de Filtros

---
+++ ProyectoModelado/lstGraficas.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoModelado
+{
+    public static class lstGraficas
+    {
+        public static int[] Grises = new int[256];
+        public static int[] Red = new int[256];
+        public static int[] Green = new int[256];
+        public static int[] Blue = new int[256];
+    }
+}
