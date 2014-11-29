7a0307ba30545e7a8cf2a1ac179e8872d019c7cd
gcachuo <gcachu.o@gmail.com>
2014-11-28 21:03:30 -0600

    VideoKinect
    video a color de kinect

--- ProyectoModelado/Form1.cs
+++ ProyectoModelado/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.Integration;
 
 namespace ProyectoModelado
 {
@@ -15,6 +16,16 @@ namespace ProyectoModelado
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Form1 form =new Form1();
+            form.Hide();
+            var wpf = new Main1();
+            ElementHost.EnableModelessKeyboardInterop(wpf);
+            wpf.Show();
         }
     }
 }
---
+++ ProyectoModelado/Main.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+
+namespace ProyectoModelado
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main1
+    {
+        public Main1()
+        {
+            InitializeComponent();
+        }
+
+        private KinectSensor sensor;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sensor = KinectSensor.KinectSensors[0];
+            sensor.Start();
+            sensor.ColorStream.Enable();
+            sensor.ColorFrameReady += sensor_ColorFrameReady;
+        }
+
+        void sensor_ColorFrameReady(object sensor, ColorImageFrameReadyEventArgs e)
+        {
+            using (var frameImagen = e.OpenColorImageFrame())
+            {
+                if (frameImagen == null) return;
+                var datosColor=new byte[frameImagen.PixelDataLength];
+
+                frameImagen.CopyPixelDataTo(datosColor);
+                mostrarVideo.Source = BitmapSource.Create(
+                    frameImagen.Width,frameImagen.Height,
+                    96,
+                    96,
+                    PixelFormats.Bgr32,
+                    null,
+                    datosColor,
+                    frameImagen.Width * frameImagen.BytesPerPixel
+                    );
+            }
+        }
+    }
+}
